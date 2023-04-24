1bcce32bf653c1992601b7aa64950ad265e9bf86
Matt <matt.cavallo56@gmail.com>
2023-04-23 21:24:34 -0500

    Working version using latitude and longitude in POST

--- BackgroundLocation2/LocationManager.swift
+++ BackgroundLocation2/LocationManager.swift
@@ -1,10 +1,12 @@
 import Foundation
+import SwiftUI
 import CoreLocation
 import Combine
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var currentLocation: CLLocation?
     private let locationManager = CLLocationManager()
+    private var counter = 0
 
     override init() {
         super.init()
@@ -27,6 +29,71 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private func logLocation(location: CLLocation) {
         // Save the location data or perform any background tasks with the updated location here.
         // For example, you could save the location data to a local database or send it to a server.
-        print("Logging")
+        print(location)
+        
+        if (counter > 59)
+        {
+            let locationData = [
+                "latitude": location.coordinate.latitude,
+                "longitude": location.coordinate.longitude
+            ]
+            
+            let jsonData = try? JSONSerialization.data(withJSONObject: locationData)
+            
+            guard let url = URL(string: "https://locationfunc56.azurewebsites.net/api/HttpTrigger1") else {
+                print("Invalid URL")
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let data = data {
+                    if let responseString = String(data: data, encoding: .utf8) {
+                        print("Response: \(responseString)")
+                    }
+                } else if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                }
+            }.resume()
+            
+            counter = 0
+        }
+        
+        counter += 1
+        
+//        guard let url = URL(string: "https://locationfunc56.azurewebsites.net/api/HttpTrigger1") else { print("invalid"); return }
+        
+//        var request = URLRequest(url: url)
+//        request.httpMethod = "POST"
+//        var lat = location.coordinate.latitude
+//
+//        let coords = Text(String(format: "%.4f, %.4f", location.coordinate.latitude, location.coordinate.longitude))
+//
+//        if let data = coords. .content.rawValue {
+//            // Do something with the data
+//            print("Data: \(data)")
+//        }
+//
+//        request.httpBody = data
+        
+        
+//        URLSession.shared.dataTask(with: request) { data, response, error in
+//            if let data = data {
+//                if let responseString = String(data: data, encoding: .utf8) {
+//                    print("Response: \(responseString)")
+//                }
+//            } else if let error = error {
+//                print("Error: \(error.localizedDescription)")
+//            }
+//        }.resume()
+    }
+    
+    private func sendLocationInfo() {
+        
+        
+
     }
 }
